9eba3a5a31b4881d86ef2031ef20f89cf4b4799d
Taha Asif <tahaasifaqwe@gmail.com>
2026-06-23 08:06:59 +0500

    remove neuctra ui api

--- neuctra-ui-api/routes/uiRoutes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { generateUI } from "../controllers/uiController.js";
-
-const router = Router();
-
-// Define routes for UI Agent
-router.post("/generate", generateUI);
-
-export default router;
